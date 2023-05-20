eda7212603ee694ebf7802b12076b4fd02280595
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2023-05-20 23:19:16 +0200

    save and load messages with mongodb

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
 const ws = require("ws")
 const http = require("http")
 const express = require("express")
+const {MongoClient, ServerApiVersion} = require("mongodb")
+
+require("dotenv").config()
 
 const port = process.env.PORT || 3000
+const logging = process.env.LOGGING
 
 let users = []
 let rooms = {}
 
+const mongoUserName = process.env.MONGODBUSERNAME
+const mongoPassword = process.env.MONGODBPASSWORD
+const mongoClusterName = process.env.MONGODBCLUSTERNAME
+const mongoURLEnd = process.env.MONGODBURLEND
+const mongoDataBase = process.env.HISTORYDB || "prod"
+const mongoCol = process.env.HISTORYCOL || "latestMessages"
+let messageHistoryLength
+let mongoURI
+if(mongoClusterName && mongoPassword && mongoUserName && mongoURLEnd) {
+    messageHistoryLength = process.env.HISTORYLENGTH || 10
+    mongoURI = `mongodb+srv://${mongoUserName}:${mongoPassword}@${mongoClusterName}.${mongoURLEnd}.mongodb.net/?retryWrites=true&w=majority`
+} else {
+    messageHistoryLength = 0
+}
+
+
+if(messageHistoryLength) {
+    if(logging) console.log(`A message history of ${messageHistoryLength} messages will be kept on the mongodb cluster ${`mongodb+srv://${mongoUserName}:<password>@${mongoClusterName}.${mongoURLEnd}.mongodb.net/?retryWrites=true&w=majority`}`)
+    if(logging) console.log(`Messages will be kept in the database "${mongoDataBase}" under the collection "${mongoCol}"`)
+    if(mongoPassword) {
+        console.log("a password is present, but wont be logged for obvious security reasons.")
+    } else {
+        console.log("YOU FORGOT SETTING THE \"MONGODBPASSWORD\" ENVIROMENT VARIABLE.")
+    }
+} else {
+    if(logging) {
+        console.log("tip: did you know you can use this app connected to a mongodb databese for message persistence?")
+    }
+}
+
 const app = express()
 
 const server = http.createServer(app)
-
-require("dotenv").config()
 
 
 const wss = new ws.Server({ server })
@@ -37,12 +69,7 @@ wss.on("connection", ws => {
                 } else {
                     rooms[message.login.room] = [ws]
                 }
-                console.log("registered User \"" + message.login.userName + "\" in room \"" + message.login.room + "\"")
-                ws.send(JSON.stringify({
-                    "message": `You connected to the room "${message.login.room}" as "${message.login.userName}". There are now ${rooms[message.login.room].length} user(s) connected to this room.`,
-                    "sender": "TheBaum's messaging server",
-                    "time": new Date()
-                }))
+                if(logging) console.log("registered User \"" + message.login.userName + "\" in room \"" + message.login.room + "\"")
                 rooms[message.login.room].forEach(user => {
                     if(user.OPEN && user != ws)
                     user.send(JSON.stringify({
@@ -52,10 +79,57 @@ wss.on("connection", ws => {
                     }))
                 })
             }
+
+            if(messageHistoryLength) {
+                let previousMessages = []
+
+                const mongoClient = new MongoClient(mongoURI, {
+                    serverApi: {
+                        version: ServerApiVersion.v1,
+                        strict: true,
+                        deprecationErrors: true,
+                    }
+                })
+
+                async function queryDatabase() {
+                    try {
+                        const database = mongoClient.db(mongoDataBase)
+                        const col = database.collection(mongoCol)
+
+                        const query = {room: message.login.room}
+                        let messagesFromRoom = await col.findOne(query)
+
+                        if(messagesFromRoom) {
+                            previousMessages = messagesFromRoom.messages
+                            previousMessages.forEach(elem => {
+                                ws.send(JSON.stringify({
+                                    "message": elem.message,
+                                    "sender": elem.sender,
+                                    "time": elem.time
+                                }))
+                            })
+                        }
+                        
+                        ws.send(JSON.stringify({
+                            "message": `You connected to the room "${message.login.room}" as "${message.login.userName}". There are now ${rooms[message.login.room].length} user(s) connected to this room.`,
+                            "sender": "TheBaum's messaging server",
+                            "time": new Date()
+                        }))
+                    } finally {
+                        await mongoClient.close()
+                    }
+                }
+
+                queryDatabase().catch(console.dir)
+            } else {
+                ws.send(JSON.stringify({
+                    "message": `You connected to the room "${message.login.room}" as "${message.login.userName}". There are now ${rooms[message.login.room].length} user(s) connected to this room.`,
+                    "sender": "TheBaum's messaging server",
+                    "time": new Date()
+                }))
+            }
         }
     })
-
-    //TODO: send and load last 10 messages to and from database
 
     ws.send(JSON.stringify({
         message:"Welcome to TheBaum's messaging server.",
@@ -65,14 +139,16 @@ wss.on("connection", ws => {
 
     ws.on("message", (message, isBinary) => {
         const index = users.findIndex(e => e.ws === ws);
-        if (index > -1) {
+        if (index > -1 && !JSON.parse(message.toString()).login) {
+
+            let messageToSave = JSON.parse(message.toString())
+            messageToSave["sender"] = users[index].userName
+
+            if(messageHistoryLength){updateDatabase(users[index].room, messageToSave)}
+
             users.forEach(user => {
                 if(user.ws != ws && user.ws.OPEN && user.room == users[index].room && !JSON.parse(message.toString()).login) {
-                    user.ws.send(JSON.stringify({
-                        "message": JSON.parse(message.toString()).message,
-                        "time": JSON.parse(message.toString()).time,
-                        "sender": users[index].userName
-                    }), {binary: isBinary})
+                    user.ws.send(JSON.stringify(messageToSave), {binary: isBinary})
                 }
             })
         }
@@ -94,7 +170,7 @@ wss.on("connection", ws => {
                     "time": new Date()
                 }))
             })
-            console.log(`removed User "${leavingUserName}" from room "${roomLeft}"`)
+            if(logging) console.log(`removed User "${leavingUserName}" from room "${roomLeft}"`)
         }
     })
 
@@ -107,3 +183,57 @@ app.use(express.static("public"))
 server.listen(port, () => {
     console.log("listening on port " + port)
 })
+
+
+async function updateDatabase(room, message) {
+    const mongoClient = new MongoClient(mongoURI, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    })
+    
+    try {
+        const database = mongoClient.db(mongoDataBase)
+        const col = database.collection(mongoCol)
+
+        const filter = {room: room}
+
+        const options = {upsert: true}
+
+        const query = {room: room}
+        
+        let messagesFromRoom = await col.findOne(query)
+        let previousMessages = []
+
+        if(messagesFromRoom) {
+            previousMessages = messagesFromRoom.messages
+            if(messageHistoryLength > 1) {
+                while(previousMessages.length > messageHistoryLength - 1) {
+                    previousMessages.splice(0, 1)
+                }
+            }
+            previousMessages.push(message)
+            if(messageHistoryLength == 0) {
+                previousMessages = []
+            }
+            if(messageHistoryLength == 1) {
+                previousMessages = [message]
+            }
+        } else {
+            previousMessages = [message]
+        }
+
+        let updateDoc = {
+            $set: {
+                "messages": previousMessages
+            }
+        }
+
+        const result = await col.updateOne(filter, updateDoc, options)
+
+    } finally {
+        await mongoClient.close()
+    }
+}
